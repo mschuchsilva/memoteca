0c18f48b64d0e55b98da5b2abec63d564d452134
Mateus Schuch da Silva <mateus-silva@procergs.rs.gov.br>
2025-06-14 10:13:27 -0300

    Event Binding

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,13 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent {
+  cancelar() {
+    throw new Error('Method not implemented.');
+  }
+  criarPensamento() {
+    throw new Error('Method not implemented.');
+  }
   pensamento = {
     id: '1',
     conteudo: 'Aprendendo Angular',
     autoria: 'Dev',
-    modelo:''
+    modelo: '',
   };
 }
